60f99db844bdb52b7057ab55e9f7737234a9085d
Guillermo <guilleiguaran@gmail.com>
2010-07-20 15:07:41 -0500

    Adding ym4r-gm plugin

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "nofxx-postgis_adapter", :lib => "postgis_adapter"
+  config.gem "nofxx-georuby", :lib => "geo_ruby"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
