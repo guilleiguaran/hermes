695204b2103ba193ea2d8de14b7c0efc59033cf5
Guillermo <guilleiguaran@gmail.com>
2010-07-21 20:35:34 -0500

    Adding textfields

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,33 +1,21 @@
 class RoutesController < ApplicationController
   
   def index
-    places = ["Universidad del Norte, Barranquilla", "Estadio Metropolitano, Barranquilla"]
-    results = []
-    places.each do |place|
-      result = Geocoding::get(place)
-      if result.status == Geocoding::GEO_SUCCESS
-        results << {:latlon => result[0].latlon, :address => result[0].address}
-      end
-    end
-
     @map = GMap.new("map_div")
     @map.control_init(:large_map => true, :map_type => true)
-    @map.center_zoom_init(results[0][:latlon],13)
+    @map.center_zoom_init([10.982987,-74.799213],13)
 
-    @start_marker = GMarker.new(results[0][:latlon], :title => "Inicio", :info_window =>"Arrastre al punto de inicio", :draggable => true)
-    @end_marker   = GMarker.new(results[1][:latlon], :title => "Fin", :info_window =>"Arrastre al punto de final", :draggable => true)
+    @start_marker = GMarker.new([11.008601, -74.833717], :title => "Start", :info_window =>"Move to start", :draggable => true)
+    @end_marker   = GMarker.new([10.998996,-74.813118], :title => "End", :info_window =>"Move to end", :draggable => true)
 
     @map.declare_init(@start_marker,"start")
     @map.declare_init(@end_marker,"end")
     @map.overlay_init(@start_marker)
     @map.overlay_init(@end_marker)
-    #@map.event_init(@start_marker, :dragend,"function(){ start_marker_dragend(this); }")
-    #@map.event_init(@end_marker, :dragend,"function(){ end_marker_dragend(this); }")
-
-    @start_lat = results[0][:latlon][0]
-    @start_lon = results[0][:latlon][1]
-    @end_lat = results[1][:latlon][0]
-    @end_lon = results[1][:latlon][1]
+    @map.event_init(@start_marker, :dragend,"function(){ start_marker_dragend(this); }")
+    @map.event_init(@end_marker, :dragend,"function(){ end_marker_dragend(this); }")
+    @start_lat = "11.008601" ; @start_lon = "-74.833717"
+    @end_lat = "10.998996" ; @end_lon = "-74.813118"
   end
   
   
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function start_marker_dragend(marker){
+	if(polyline != undefined) { map.removeOverlay(polyline); }
+	var latitud_origen = document.getElementById('latitud_origen');
+	var longitud_origen = document.getElementById('longitud_origen');
+	var latitud = marker.getLatLng().lat()+'';
+	var longitud = marker.getLatLng().lng()+'';
+	latitud_origen.value = latitud.substring(0,latitud.indexOf('.')+8);
+	longitud_origen.value = longitud.substring(0,longitud.indexOf('.')+8);
+}
+
+function end_marker_dragend(marker){
+	if(polyline != undefined) { map.removeOverlay(polyline); }
+	var latitud_destino = document.getElementById('latitud_destino');
+	var longitud_destino = document.getElementById('longitud_destino');
+	var latitud = marker.getLatLng().lat()+'';
+	var longitud = marker.getLatLng().lng()+'';
+	latitud_destino.value = latitud.substring(0,latitud.indexOf('.')+8);
+	longitud_destino.value = longitud.substring(0,longitud.indexOf('.')+8);
+}
