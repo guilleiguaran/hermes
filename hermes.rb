d0e9345394723f290a9d5c7b1388401603b6bf04
Lacides Charris <lacides@lacides-laptop.(none)>
2010-09-23 19:33:50 -0500

    \closest_node y \route

--- hermes.rb
+++ hermes.rb
@@ -1,7 +1,7 @@
 require "erb"
 
 class Hermes < Sinatra::Application
-  @@pgconn = PGconn.connect "localhost", 5432, "", "", "hermes_development", "root", ""
+  @@pgconn = PGconn.connect "localhost", 5432, "", "", "Hermes", "lacho", "abc123"
   
   configure do
     set :views, "#{File.dirname(__FILE__)}/views"
@@ -18,9 +18,41 @@ class Hermes < Sinatra::Application
   end
   
   get '/route' do
-    
+		startId = params['startId']
+		goalId = params['goalId']
+		if(startId != nil && goalId != nil)
+			res = @@pgconn.exec "SELECT vertex_id, cost FROM shortest_path('
+		SELECT gid as id, 
+			 source::integer, 
+			 target::integer, 
+			 cost::double precision,
+			 reverse_cost::double precision  
+			FROM roads', 
+		#{startId}, #{goalId}, true, true);"
+			indices = res.result
+			ruta = Array.new			
+			indices.each do |fila|
+				id = fila[0]
+				res = @@pgconn.exec "SELECT ST_AsText(the_geom) as texto FROM vertices_tmp WHERE id = #{id};"
+				ruta << res.result[0][0]
+			end		
+			ruta.inspect
+		else
+			"Params missing"
+		end		
   end
   
+  get '/closest_node' do
+		lat = params['Lat'] 
+		lon = params['Lon']
+		if(lat != nil && lon != nil)
+			res = @@pgconn.exec "SELECT id,ST_AsText(the_geom) FROM vertices_tmp ORDER BY ST_Distance(the_geom, GeomFromText('POINT(#{lat} #{lon})', 21892)) LIMIT 1; "
+			res.result[0].inspect
+		else
+			"Params missing"
+		end
+  end  
+
   get '/test_query' do
     res = @@pgconn.exec "SELECT gid, id, length from roads LIMIT 10;"
     res.result.inspect
---
+++ server/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '96e54c93202bb27bc2adeb6097c88c1df45435733c74b68aa20ebfa08e3a354217fef36c1ff99a1edc612c6a17b74ffe6a611cfd1d174c91f6259e02901acb03';
