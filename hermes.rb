30f7dfb0e8e7bdbbe40a62e3aed699ec952724fa
Guillermo <guilleiguaran@gmail.com>
2011-01-11 00:49:46 -0500

    Adding icons and loading screen

--- hermes.rb
+++ hermes.rb
@@ -11,11 +11,6 @@ class Hermes < Sinatra::Application
     set :public, "#{File.dirname(__FILE__)}/public"
   end
 
-  before do
-    puts '[Params]'
-    p params
-  end
-
   get '/' do
     erb :index
   end
@@ -36,31 +31,20 @@ class Hermes < Sinatra::Application
       rows = res.result.inspect.gsub("[[","").gsub("]]","").split("], [")
       ruta = []
       rows.each do |row|
-	multiline = []
+	      multiline = []
         points = row.gsub("MULTILINESTRING((","").gsub("))","").split(",")
-	points.each do |point|
-		coord = [point.split(" ")[1].gsub("\"","").to_f, point.split(" ")[0].gsub("\"","").to_f]
-		multiline << coord	
-	end
-	ruta << multiline
+	      points.each do |point|
+		      coord = [point.split(" ")[1].gsub("\"","").to_f, point.split(" ")[0].gsub("\"","").to_f]
+		      multiline << coord	
+	      end
+	      ruta << multiline
       end
-	ruta.to_json
-
+	    ruta.to_json
     else
       "Params missing"
     end		
   end
   
-  get '/test_route' do
-    start_lat = params[:start_lat]
-    start_lon = params[:start_lon]
-    end_lat = params[:end_lat]
-    end_lon = params[:end_lon]
-    
-    route = [ [start_lat.to_f,start_lon.to_f], [end_lat.to_f, end_lon.to_f] ]
-    route.to_json
-  end
-
   def closest_node(lat, lon)
     res = @@pgconn.exec "SELECT id,ST_AsText(the_geom) FROM vertices_tmp ORDER BY ST_Distance(the_geom, GeomFromText('POINT(#{lon} #{lat})', 21892)) LIMIT 1;"
     res.result[0]
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,18 +10,23 @@ function initialize() {
 	var startCoord = new google.maps.LatLng(11.01804, -74.85037);
 	var endCoord = new google.maps.LatLng(10.92690, -74.80078);
 	
+	var startImage = '/images/dd-start.png';
+	var endImage = '/images/dd-end.png';
+	
 	var startMarker = new google.maps.Marker({
 	    position: startCoord, 
 	    map: window.map, 
 	    title: "Start Position",
-	    draggable: true
+	    draggable: true,
+		icon: startImage
 	});
 	
 	var endMarker = new google.maps.Marker({
     	position: endCoord, 
 	    map: window.map, 
 	    title: "End Position",
-	    draggable: true
+	    draggable: true,
+		icon: endImage
 	});
 	
 	google.maps.event.addListener(startMarker, 'dragend', function(event) {
@@ -55,10 +60,9 @@ function calculateRoute(startCoord, endCoord) {
 	var endLon = endCoord.lng();
 	
 	var route = new Array();
-	
+	$("#loading").addClass("loading-visible");
 	$.getJSON("/route?start_lat="+startLat+"&start_lon="+startLon+"&end_lat="+endLat+"&end_lon="+endLon, function(data) {
-		console.log(data);
-	  	$.each(data, function(i, polyline_data){
+		$.each(data, function(i, polyline_data){
 			var polyline = new Array();
 			$.each(polyline_data, function(j, coord){
 				var latlng = new google.maps.LatLng(coord[0], coord[1]);
@@ -66,9 +70,9 @@ function calculateRoute(startCoord, endCoord) {
 			});
 			var p = new google.maps.Polyline({
 			    path: polyline,
-			    strokeColor: "#0000FF",
-			    strokeOpacity: 1.0,
-			    strokeWeight: 2
+			    strokeColor: "#6633FF",
+			    strokeOpacity: 0.60,
+			    strokeWeight: 3
 			});
 			p.setMap(window.map);
 			route.push(p);
@@ -81,5 +85,7 @@ function calculateRoute(startCoord, endCoord) {
 			});
 		}
 		window.routePath = route;
+		$("#loading").removeClass("loading-visible");
 	});
+	
 }
