54180dc13e7f4bdc6ef1a8c8497f8117b3e4d13a
Guillermo <guilleiguaran@gmail.com>
2011-01-22 21:24:53 -0500

    Refactoring application.js

--- hermes.rb
+++ hermes.rb
@@ -25,14 +25,11 @@ class Hermes < Sinatra::Application
     if start_lat && goal_lat && start_lon && goal_lon
 
       start_id = closest_node(start_lat,start_lon)[0]
-      goal_id = closest_node(goal_lat,goal_lon)[0]
-
-   #   res = @@pgconn.exec("SELECT ST_AsText(the_geom) FROM dijkstra_sp_directed('roads', #{start_id}, #{goal_id},true,true) ORDER BY id DESC;")
-     
-      res = @@pgconn.exec("SELECT ST_AsText(the_geom) FROM shortest_path('SELECT gid as id, source, target, cost, reverse_cost FROM roads',#{start_id}, #{goal_id},true,true) as sp, roads WHERE gid = edge_id;");
+      goal_id = closest_node(goal_lat,goal_lon)[0]     
+      res = @@pgconn.exec("SELECT ST_AsText(the_geom) FROM shortest_path('SELECT gid as id, source, target, cost, reverse_cost FROM roads',#{start_id}, #{goal_id},true,true) as sp, roads WHERE gid = edge_id;")
 
       rows = res.result.inspect.gsub("[[","").gsub("]]","").split("], [")
-      ruta = []
+      route = []
       rows.each do |row|
 	      multiline = []
         points = row.gsub("MULTILINESTRING((","").gsub("))","").split(",")
@@ -40,9 +37,9 @@ class Hermes < Sinatra::Application
 		      coord = [point.split(" ")[1].gsub("\"","").to_f, point.split(" ")[0].gsub("\"","").to_f]
 		      multiline << coord	
 	      end
-	      ruta << multiline
+	      route << multiline
       end
-	    ruta.to_json
+	    route.to_json
     else
       "Params missing"
     end		
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,14 @@
+var map;
+var routePath = null;
+
 function initialize() {
-    var center = new google.maps.LatLng(10.9720, -74.7992);
-    var options = {
+	var center = new google.maps.LatLng(10.9720, -74.7992);
+  var options = {
 		zoom: 13,
-    	center: center,
-    	mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    window.map = new google.maps.Map(document.getElementById("map_canvas"), options);
+    center: center,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+	map = new google.maps.Map(document.getElementById("map_canvas"), options);
 
 	var startCoord = new google.maps.LatLng(11.01804, -74.85037);
 	var endCoord = new google.maps.LatLng(10.92690, -74.80078);
@@ -14,40 +17,41 @@ function initialize() {
 	var endImage = '/images/dd-end.png';
 	
 	var startMarker = new google.maps.Marker({
-	    position: startCoord, 
-	    map: window.map, 
-	    title: "Start Position",
-	    draggable: true,
+	  position: startCoord, 
+	  map: map, 
+	  title: "Start Position",
+	  draggable: true,
 		icon: startImage
 	});
 	
 	var endMarker = new google.maps.Marker({
-    	position: endCoord, 
-	    map: window.map, 
-	    title: "End Position",
-	    draggable: true,
+    position: endCoord, 
+	  map: map, 
+	  title: "End Position",
+	  draggable: true,
 		icon: endImage
 	});
 	
 	google.maps.event.addListener(startMarker, 'dragend', function(event) {
-	    calculateRoute(startMarker.getPosition(), endMarker.getPosition());
+		calculateRoute(startMarker.getPosition(), endMarker.getPosition());
 	});
+	
 	google.maps.event.addListener(endMarker, 'dragend', function(event) {
-	    calculateRoute(startMarker.getPosition(), endMarker.getPosition());
+	  calculateRoute(startMarker.getPosition(), endMarker.getPosition());
 	});
 	
 	calculateRoute(startMarker.getPosition(), endMarker.getPosition());
 	
 	var userAgent = navigator.userAgent.toLowerCase();
 	
-	if (navigator.geolocation) {
+	if(navigator.geolocation) {
 	  navigator.geolocation.getCurrentPosition(function(position) {
-		var lat = position.coords.latitude;
-	    var lng = position.coords.longitude;
-	    var myLatlng = new google.maps.LatLng(lat, lng);
-		startMarker.setPosition(myLatlng)
-	    window.map.setCenter(myLatlng);
-		calculateRoute(startMarker.getPosition(), endMarker.getPosition());
+			var lat = position.coords.latitude;
+	  	var lng = position.coords.longitude;
+	  	var myLatlng = new google.maps.LatLng(lat, lng);
+			startMarker.setPosition(myLatlng)
+	  	map.setCenter(myLatlng);
+			calculateRoute(startMarker.getPosition(), endMarker.getPosition());
 	  });
 	}
 
@@ -59,33 +63,36 @@ function calculateRoute(startCoord, endCoord) {
 	var endLat = endCoord.lat();
 	var endLon = endCoord.lng();
 	
+	//$("#loading").addClass("loading-visible");
+	document.getElementById("loading").className = "loading-visible";
 	var route = new Array();
-	$("#loading").addClass("loading-visible");
-	$.getJSON("/route?start_lat="+startLat+"&start_lon="+startLon+"&end_lat="+endLat+"&end_lon="+endLon, function(data) {
-		$.each(data, function(i, polyline_data){
-			var polyline = new Array();
-			$.each(polyline_data, function(j, coord){
-				var latlng = new google.maps.LatLng(coord[0], coord[1]);
-				polyline.push(latlng);
-			});
+	jx.load("/route?start_lat="+startLat+"&start_lon="+startLon+"&end_lat="+endLat+"&end_lon="+endLon, function(data) {
+
+		for(var i = 0; i < data.length; i++){
+			var coords = new google.maps.MVCArray();
+			for(var j = 0; j < data[i].length; j++)
+			{
+				coords.insertAt(i, new google.maps.LatLng(data[i][j][0], data[i][j][1]))
+			}
 			var p = new google.maps.Polyline({
-			    path: polyline,
-			    strokeColor: "#6633FF",
-			    strokeOpacity: 0.60,
-			    strokeWeight: 3
+				path: coords,
+				strokeColor: "#6633FF",
+				strokeOpacity: 0.60,
+				strokeWeight: 3
 			});
-			p.setMap(window.map);
+			p.setMap(map);
 			route.push(p);
-		});
-		
-		if(window.routePath != null)
-		{
-			$.each(routePath, function(i, routeItem) { 
-			  routeItem.setMap(null);
-			});
 		}
-		window.routePath = route;
-		$("#loading").removeClass("loading-visible");
-	});
-	
+		if (routePath != null)
+		{
+			for(var i = 0; i < routePath.length; i++)
+			{
+				routePath[i].setMap(null);
+			}
+		}
+		routePath = route;
+		
+		//$("#loading").removeClass("loading-visible");
+	},'json');
+  document.getElementById("loading").className = "loading-invisible";
 }
